da5c19cb806f114941d9cbbe8685d943ec5c8c9a
zz <zz.at.field@gmail.com>
2014-12-13 15:26:23 +0800

    rewrite propmt

--- lib/prompt.py
+++ lib/prompt.py
@@ -1,25 +1,19 @@
 __author__ = 'zz'
 
 from threading import Lock
-
-from random import choice
+from random import choice as random_choice
 from functools import wraps, partial
-from .decorators import threading_lock, prefix_print
 from shutil import get_terminal_size
-from contextlib import contextmanager
+import types
 
-error_lock = Lock()
-prompt_lock = Lock()
-prompt_detect_error_lock = Lock()
-test_lock =Lock()
-terminal_width, _ = get_terminal_size()
+TERMINAL_WIDTH, _ = get_terminal_size()
 
 
 
 def clear_output(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        print(' ' * terminal_width, end='\r')
+        print(' ' * TERMINAL_WIDTH, end='\r')
         return func(*args, **kwargs)
     return wrapper
 
@@ -37,6 +31,11 @@ class Colors:
 
 
 def shellcolor(color: str, func=None):
+    """
+     Available color:
+     header, okblue, okgreen, warning, fail
+    """
+
     if func is None:
         return partial(shellcolor, color)
 
@@ -48,11 +47,77 @@ def shellcolor(color: str, func=None):
     def wrapper(*args, **kwargs):
         print(color_prefix, end='')
         ret = func(*args, **kwargs)
-        # func use print, there will be a '\n', so must use '\r' write back.
+
+        # func use print, there will be a '\n', so must use '\r' write then  back.
         print(color_suffix, end='\r')
 
         return ret
     return wrapper
+
+
+class Prompt:
+
+    prompt_text = (
+        '正在检查线路安全',
+        '正在确保控制器',
+        '重载思考模型',
+        '正在检查插入栓深度',
+        '正在检查裙子里面的不明突起',
+        '正在解决自涉悖论',
+        '正在展开AT力场',
+        '正在确保线路057',
+        '正在启动过载保护',
+        '魔力控制在预期范围内',
+        '零宽度负预测先行断言 启动',
+        '链式回环加速 启动',
+
+    )
+
+    def __init__(self, func):
+        wraps(func)(self)
+        self._lock = Lock()
+        self.terminal_width = TERMINAL_WIDTH
+
+        # text which will print
+        self._current_text = self._random_text
+
+        # must use self.reset() to set below args
+        self._ncalls = 0
+        self._total = 0
+
+    @clear_output
+    def __call__(self, *args, **kwargs):
+        with self._lock:
+            self._ncalls += 1
+            print(format(self._ncalls / self._total, '<8.2%') + self._current_text, end='\r')
+
+            if self._ncalls % 4 == 0:
+                self._current_text = self._random_text
+
+        return self.__wrapped__(*args, **kwargs)
+
+    @property
+    def _random_text(self):
+        return random_choice(self.prompt_text)
+
+    @shellcolor('header')
+    def set_task(self, total, header):
+        '''
+        total, header
+        设置中的任务数, 输出的标题.
+        '''
+        self._total = total
+        self._ncalls = 0
+        print(header)
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        else:
+            return types.MethodType(self, instance)
+
+
+
 
 
 # class Color(bcolors):
